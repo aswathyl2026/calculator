a85d64088b0414f57af9ed212295ea7160668f28
aswathyl2026 <aswathyrepository@gmail.com>
2026-02-20 11:14:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//display calc
+const calcDisp=(content)=>{
+result.value+=content
+}
+
+//clear display
+const clearDisp=()=>{
+result.value=""
+}
+
+//backspace
+const backspace=()=>{
+result.value=result.value.slice(0,-1)
+}
+
+//calculate result
+const calcResult=()=>{
+    try{
+result.value=eval(result.value)
+    }catch(err){
+        result.value=""
+result.value="Invalid Expression"
+    }
+}
